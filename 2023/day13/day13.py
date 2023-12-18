0f4f4a6578ace7037568d8fe0524b86be2e36af8
Paul Kasemir <paul.kasemir@gmail.com>
2023-12-18 00:21:36 -0700

    [day13] Solve both puzzles

---
+++ 2023/day13/day13.py
@@ -0,0 +1,144 @@
+import unittest
+import sys
+import time
+import os
+import re
+
+from pprint import pprint, pformat
+
+class TimeBlock:
+    def __init__(self, description=None):
+        self.description = description
+
+    def __enter__(self):
+        self.begin = time.time()
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        end = time.time()
+        if exception_type is not None:
+            raise exception_value
+
+        print(f"{'' if self.description is None else self.description + ' '}took {end - self.begin:.3f} sec")
+
+class Input(object):
+    def __str__(self):
+        return f"<Input {pformat(self.__dict__)}>"
+
+    __repr__ = __str__
+
+def convert_input(line):
+    input = Input()
+    input.line = line
+    return line
+
+def load_input(filename, solution_p1=None, solution_p2=None):
+    try:
+        with open(filename,'r') as f:
+            lines = [line.strip() for line in f.readlines()]
+    except:
+        print("No input file", filename)
+        return None
+    inputs = [convert_input(line) for line in lines]
+    return (inputs, solution_p1, solution_p2)
+
+def intersect_mirror_location(mirror_dict: dict, threshold, value):
+    for idx in list(mirror_dict.keys()):
+        left = reversed(value[:idx])
+        right = value[idx:]
+        mirror_dict[idx] += sum([1 for r, l in zip(right, left) if r != l])
+
+        if mirror_dict[idx] > threshold:
+            del mirror_dict[idx]
+    #     print(value, idx, left, right)
+    # print(mirror_dict)
+
+def find_mirror_value(threshold, pattern):
+    # print("Check pattern")
+    mirror_dict = {idx:0 for idx in range(1, len(pattern[0]))}
+    for row in pattern:
+        if len(mirror_dict) == 0:
+            break
+        intersect_mirror_location(mirror_dict, threshold, row)
+
+    mirror_dict = dict(filter(lambda item: item[1] == threshold, mirror_dict.items()))
+
+    if len(mirror_dict) == 1:
+        return list(mirror_dict.keys())[0]
+
+    # print('check col wise')
+    mirror_dict = {idx:0 for idx in range(1, len(pattern))}
+    for x in range(len(pattern[0])):
+        if len(mirror_dict) == 0:
+            break
+        col = "".join([pattern[y][x] for y in range(len(pattern))])
+        # print ('col', x, col)
+        intersect_mirror_location(mirror_dict, threshold, col)
+
+    mirror_dict = dict(filter(lambda item: item[1] == threshold, mirror_dict.items()))
+
+    if len(mirror_dict) == 1:
+        return 100 * list(mirror_dict.keys())[0]
+
+    print("Mirror not found")
+    return 0
+
+def part1(input):
+    total = 0
+
+    pattern = []
+    for row in input:
+        if len(row) == 0:
+            total += find_mirror_value(0, pattern)
+            pattern = []
+        else:
+            pattern.append(row)
+
+    if len(pattern) > 0:
+        total += find_mirror_value(0, pattern)
+
+
+    return total
+
+def part2(input):
+    total = 0
+    pattern = []
+    for row in input:
+        if len(row) == 0:
+            total += find_mirror_value(1, pattern)
+            pattern = []
+        else:
+            pattern.append(row)
+
+    if len(pattern) > 0:
+        total += find_mirror_value(1, pattern)
+
+    return total
+
+class AdventOfCode(unittest.TestCase):
+    def setUp(self) -> None:
+        script_dir = os.path.dirname(sys.argv[0])
+        if len(script_dir) > 0:
+            script_dir += '/'
+
+        self.input_set = [
+            load_input(script_dir + "example1.txt", 405, 400),
+            load_input(script_dir + "input1.txt"),
+        ]
+
+    def run_part(self, part_num, part_func):
+        self.assertIn(part_num, [1, 2])
+        for input_set in self.input_set:
+            self.assertIsNotNone(input_set)
+            expect = input_set[part_num]
+            answer = part_func(input_set[0])
+            print(f"Part{part_num} result", answer)
+            if expect is not None:
+                self.assertEqual(expect, answer)
+
+    def test_part1(self):
+        self.run_part(1, part1)
+
+    def test_part2(self):
+        self.run_part(2, part2)
+
+unittest.main(verbosity=0)
