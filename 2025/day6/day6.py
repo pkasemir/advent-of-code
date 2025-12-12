54cbe7d7df2efc7d09776fcfb74c930c063d8ae2
Paul Kasemir <paul.kasemir@gmail.com>
2025-12-12 00:27:09 -0700

    [day6] Solve both puzzles

--- 2025/day6/day6.py
+++ 2025/day6/day6.py
@@ -34,21 +34,72 @@ class Input(object):
 def load_input(filename, solution_p1=None, solution_p2=None):
     try:
         with open(filename,'r') as f:
-            lines = [line.strip() for line in f.readlines()]
+            lines = [line.rstrip('\r\n') for line in f.readlines()]
     except:
         print("No input file", filename)
         return None
     inputs = [Input(line) for line in lines]
     return (inputs, solution_p1, solution_p2)
 
+def op_fn(op, a, b):
+    if op == '+':
+        return a + b
+    else:
+        return a * b
+
+class WS:
+    class Set:
+        def __init__(self, numbers, op):
+            self.numbers = numbers
+            self.op = op
+
+        def __str__(self):
+            return f'{self.op} {self.numbers}'
+        __repr__ = __str__
+
+    def __init__(self, inputs: List[Input]):
+        numbers = [list(map(int, i.line.split())) for i in inputs[:-1]]
+        operations = inputs[-1].line.split()
+        self.sets1 = [self.Set(n, op) for n, op in zip(zip(*numbers), operations)]
+
+        # And for part 2
+        longest = max([len(i.line) for i in inputs])
+        equalized_lines = [i.line + (' ' * (longest - len(i.line))) for i in inputs]
+        number_lines = list(map(''.join, zip(*equalized_lines)))
+        self.sets2 = []
+        numbers = []
+        for line in number_lines:
+            if line.strip() == '':
+                self.sets2.append(self.Set(numbers, op))
+                numbers = []
+                continue
+
+            if line[-1] != ' ':
+                op = line[-1]
+            numbers.append(int(line[:-1]))
+        self.sets2.append(self.Set(numbers, op))
+
+    def solve_worksheet(self, sets: List[Set]):
+        total = 0
+        for s in sets:
+            value = s.numbers[0]
+            for n in s.numbers[1:]:
+                value = op_fn(s.op, value, n)
+            total += value
+        return total
+
 def part1(inputs: List[Input]):
     total = 0
-    for input in inputs:
-        print(input)
+    ws = WS(inputs)
+    # pprint(ws.sets1)
+    total = ws.solve_worksheet(ws.sets1)
     return total
 
 def part2(inputs: List[Input]):
     total = 0
+    ws = WS(inputs)
+    # pprint(ws.sets2)
+    total = ws.solve_worksheet(ws.sets2)
     return total
 
 class AdventOfCode(unittest.TestCase):
@@ -58,7 +109,7 @@ class AdventOfCode(unittest.TestCase):
             script_dir += '/'
 
         self.input_set = [
-            load_input(script_dir + "example1.txt", -1, -1),
+            load_input(script_dir + "example1.txt", 4277556, 3263827),
             load_input(script_dir + "input1.txt"),
         ]
 
